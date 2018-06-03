7210b55a8410bd021a1d6c28177fa23dd7f7c89d
Vachiravit <vachiravit.va@gmail.com>
2018-06-03 20:56:55 +0700

    before go to section 8

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
-
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   root  'users#new'
   resources :users
